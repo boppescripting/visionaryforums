1e68c8a7c3ed18b6ce413a4a55092f2dc2d143af
Robert Trainor <44984907+trainordevs@users.noreply.github.com>
2022-07-23 21:36:31 -0500

    Basics
    Removed default code/assets, implemented a sample navigation.

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = (props) => (
+    <nav>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>Profile</a></li>
+        </ul>
+    </nav>
+);
+
+export default Nav;
