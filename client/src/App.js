690d8476c475818af53635ac75ea0fdae2260547
Robert Trainor <44984907+trainordevs@users.noreply.github.com>
2022-07-24 14:14:17 -0500

    Core backend work
    Added Axios, created root directory nodejs app to host both the client and server side, integrated an example use of states and usage of the backend api.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Axios from "axios";
 
 // Components
 import Nav from './components/Nav';
 
 function App() {
+  const [userCount, setUserCount] = useState(0);
+
+  const insert = () => {
+    const const_vals = ['test', 'testPassword', 'testEmail']
+    Axios.post(process.env.REACT_APP_APILINK + 'insert/user', {values: const_vals}).then(() => {})
+    setUserCount((prev) => prev + 1)
+  }
+
+  useEffect(() => {
+    Axios.get(process.env.REACT_APP_APILINK + 'get/users').then((response) => {
+      setUserCount(response.data.length)
+    });
+  }, [])
+
   return (
     <div className="App">
-      <Nav />
+      {/* <Nav /> */}
+      <p>User Count: {userCount}</p>
+      <button onClick={insert}>Add user</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'visionaryforums'
+});
+
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+app.use(cors(corsOpts));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/get/users', (req, res) => {
+  const sqlQuery = "SELECT * FROM users"
+  db.query(sqlQuery, [], (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post('/api/insert/user', (req, res) => {
+    const values = req.body.values
+
+    const sqlInsert = "INSERT INTO users (username, password, email) VALUES (?);";
+    db.query(sqlInsert, [values], (err, result) => {});
+});
 
 app.listen(3001, () => {
     console.log('running')
